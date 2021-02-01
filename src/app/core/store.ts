befe349ce5d9cb85c37e8ed8ebaab78b664937d8
Anuj Uppal <anujuppal10@gmail.com>
2021-02-02 02:42:01 +0530

    Created Store

---
+++ src/app/core/store.ts
@@ -0,0 +1,25 @@
+import { BehaviorSubject, Observable } from "rxjs";
+
+export class Store<T> {
+    state$ : Observable<T>;
+    private _state$ : BehaviorSubject<T>;
+
+    constructor(initialState: T){
+        this._state$ = new BehaviorSubject<T>(initialState);
+        this.state$ = this._state$.asObservable()
+    }
+
+    get state(){
+        return this._state$.getValue;
+    }
+
+    protected setState(nexState: T): void {
+        console.log('---------------------------');
+        console.log('Previous State', this.state);
+
+        this._state$.next(nexState);
+        
+        console.log('Current State', this.state);
+        console.log('---------------------------');
+    }
+}
