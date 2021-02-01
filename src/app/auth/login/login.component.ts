c61ba0e2380f792eb69b6611478371a31d33c4bd
Anuj Uppal <anujuppal10@gmail.com>
2021-02-01 14:37:51 +0530

    Updated Login component to reactive

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'sau-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
   email: string;
   password: string;
-  error: string;
+  error: BehaviorSubject<string>;
   constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.error = new BehaviorSubject('');
   }
 
   login(){
-    this.error = '';
+    this.setError('');
     this.authService.login(this.email,this.password).subscribe(s => this.router.navigate(['']),
       e => (
-        this.error = e
+        this.setError(e)
       )
     );
+  }
+
+
+
+
+  private setError(msg: any) {
+    this.error.next(msg);
   }
 }
