0b5d4c4755f5284bf6434fe66060844bcb832211
Anuj Uppal <anujuppal10@gmail.com>
2021-02-02 02:26:55 +0530

    Added Filter and pagination

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ProductDataService } from '@core/index';
 import { MatTableDataSource } from '@angular/material/table';
 import { Product } from '@core/products/product';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'sau-products',
@@ -15,20 +16,27 @@ export class ProductsComponent implements OnInit, OnDestroy {
   subscriptions = [];
   displayedColumns = ['imgUrl', 'name', 'price', 'action'];
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private productDataService: ProductDataService) { }
 
   ngOnInit(): void {
     this.subscriptions.push(this.productDataService.getAllProducts().subscribe(products => this.onDataLoad(products)));
   }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(s => s.unsubscribe());
   }
 
-  onDataLoad(products) {
+  onDataLoad(products: Product[]) {
     this.loading = false;
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
     this.dataSource.data = products;
   }
 
